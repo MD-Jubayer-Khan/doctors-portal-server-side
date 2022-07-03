ab091552c35c5b8d4389ae7010874ab97e7522c4
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-07-03 17:50:03 +0600

    setup a simple server and connect with mongodb

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://doctors_admin:<password>@cluster1.my6ni.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('db connected');
+  // perform actions on the collection object
+  client.close();
+});
+
+app.get('/', (req, res)=> {
+    res.send('Hello from doctor uncle')
+});
+
+app.listen(port, ()=>  {
+    console.log(`Doctors app listening port ${port}`);
+})
+
+
+
