10cb320e68b6525eaad2634543aff86f24fb42d2
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-07-03 18:31:17 +0600

    create a get api

--- index.js
+++ index.js
@@ -8,14 +8,27 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://doctors_admin:<password>@cluster1.my6ni.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.my6ni.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('db connected');
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run () {
+    try{
+        await client.connect();
+        const serviceCollection = client.db('doctors_portal').collection('services');
+
+        app.get('/service', async(req, res)=>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+ 
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res)=> {
     res.send('Hello from doctor uncle')
