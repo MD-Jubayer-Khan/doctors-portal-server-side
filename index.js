1497832d2f88db6adef93f301bb5d0240855b12c
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-07-06 16:53:23 +0600

    created a post api to send booking data to database

--- index.js
+++ index.js
@@ -15,12 +15,26 @@ async function run () {
     try{
         await client.connect();
         const serviceCollection = client.db('doctors_portal').collection('services');
+        const bookingCollection = client.db('doctors_portal').collection('booking');
 
         app.get('/service', async(req, res)=>{
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services)
+        });
+
+        app.post('/booking', async(req, res) =>{
+            const booking = req.body;
+            const query = {treatment: booking.treatment, date: booking.date, patient: booking.patient};
+            const exist = await bookingCollection.findOne(query);
+
+            if(exist){
+                return res.send ({success: false, booking: exist})
+            }
+
+            const result = await bookingCollection.insertOne(booking);
+            res.send({success: true, result})
         })
  
     }
