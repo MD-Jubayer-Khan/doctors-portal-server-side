38a2b18794fbc4b067a16f292549efc91b322d4c
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-07-12 13:06:19 +0600

    create another api to load user specific data in the dashboard

--- index.js
+++ index.js
@@ -48,6 +48,13 @@ async function run () {
            
       
             res.send(services);
+          });
+
+          app.get('/booking', async(req, res) =>{
+            const patient = req.query.patient;
+            const query = {patient: patient};
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings);
           })
 
         app.post('/booking', async(req, res) =>{
