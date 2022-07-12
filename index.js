fc26fd8a15b920c4e077882abcb7ea8781c2dbfb
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-07-12 11:56:45 +0600

    created a get api

--- index.js
+++ index.js
@@ -24,6 +24,32 @@ async function run () {
             res.send(services)
         });
 
+        app.get('/available', async(req, res) =>{
+            const date = req.query.date;
+      
+            // step 1:  get all services
+            const services = await serviceCollection.find().toArray();
+      
+            // step 2: get the booking of that day. output: [{}, {}, {}, {}, {}, {}]
+            const query = {date: date};
+            const bookings = await bookingCollection.find(query).toArray();
+      
+            // step 3: for each service
+            services.forEach(service=>{
+              // step 4: find bookings for that service. output: [{}, {}, {}, {}]
+              const serviceBookings = bookings.filter(book => book.treatment === service.name);
+              // step 5: select slots for the service Bookings: ['', '', '', '']
+              const bookedSlots = serviceBookings.map(book => book.slot);
+              // step 6: select those slots that are not in bookedSlots
+              const available = service.slots.filter(slot => !bookedSlots.includes(slot));
+              //step 7: set available to slots to make it easier 
+              service.slots = available;
+            });
+           
+      
+            res.send(services);
+          })
+
         app.post('/booking', async(req, res) =>{
             const booking = req.body;
             const query = {treatment: booking.treatment, date: booking.date, patient: booking.patient};
