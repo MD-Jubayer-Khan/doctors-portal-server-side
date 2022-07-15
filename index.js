4807f8873886608f4c88edd2bc9c2f32247a3652
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-07-15 16:59:21 +0600

    created a middle tiare to verify jwt token

--- index.js
+++ index.js
@@ -12,6 +12,21 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.my6ni.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run () {
     try{
         await client.connect();
@@ -24,6 +39,11 @@ async function run () {
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services)
+        });
+
+        app.get('/user', verifyJWT, async (req, res) => {
+          const users = await userCollection.find().toArray();
+          res.send(users);
         });
 
         app.put('/user/:email', async (req, res) => {
@@ -65,11 +85,17 @@ async function run () {
             res.send(services);
           });
 
-          app.get('/booking', async(req, res) =>{
+          app.get('/booking', verifyJWT, async (req, res) => {
             const patient = req.query.patient;
-            const query = {patient: patient};
-            const bookings = await bookingCollection.find(query).toArray();
-            res.send(bookings);
+            const decodedEmail = req.decoded.email;
+            if (patient === decodedEmail) {
+              const query = { patient: patient };
+              const bookings = await bookingCollection.find(query).toArray();
+              return res.send(bookings);
+            }
+            else {
+              return res.status(403).send({ message: 'forbidden access' });
+            }
           })
 
         app.post('/booking', async(req, res) =>{
