472f0f7a3cd31330d1d2a48240ad7983ec860c22
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-07-15 17:15:46 +0600

    created a api to make user as an admin

--- index.js
+++ index.js
@@ -46,6 +46,24 @@ async function run () {
           res.send(users);
         });
 
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+          const email = req.params.email;
+          const requester = req.decoded.email;
+          const requesterAccount = await userCollection.findOne({ email: requester });
+          if (requesterAccount.role === 'admin') {
+            const filter = { email: email };
+            const updateDoc = {
+              $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+          }
+          else{
+            res.status(403).send({message: 'forbidden'});
+          }
+    
+        })
+
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
